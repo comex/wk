83d885add828c0dfeceb2e549e9fbdb4341ae746
comex <comexk@gmail.com>
2019-09-30 23:32:42 -0700

    stuff i didn't add as always

---
+++ read-english.zsh
@@ -0,0 +1,14 @@
+#!/usr/local/bin/zsh
+bad=0
+function diaf() {
+    bad=1
+    exit
+}
+export LANG=en-US.utf-8
+zle -N diaf
+bindkey '^D' diaf
+
+line=
+vared -p "$1" line
+test "$bad" = "1" && exit 1
+echo "$line"
