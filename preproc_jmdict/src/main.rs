a48692597b08ad60a4bbf57e7abbf3ff0d7300a8
comex <comexk@gmail.com>
2021-02-14 21:21:25 -0500

    Revert "quick-xml does not work"
    This reverts commit a95b313bd4130440b99a3d8ab3a7156ee421d0a9.

--- preproc_jmdict/src/main.rs
+++ preproc_jmdict/src/main.rs
@@ -1,36 +1,16 @@
-use serde::Deserialize;
-#[derive(Debug, Deserialize, PartialEq)]
-struct KEle {
-    keb: String,
-}
-#[derive(Debug, Deserialize, PartialEq)]
-struct REle {
-    reb: String,
-}
-#[derive(Debug, Deserialize, PartialEq)]
-struct Gloss {
-    g_type: Option<String>,
-    #[serde(rename = "$value")]
-    value: String,
-}
-#[derive(Debug, Deserialize, PartialEq)]
-struct Sense {
-    pos: Vec<String>,
-    xref: Vec<String>,
-    gloss: Vec<Gloss>,
-}
-#[derive(Debug, Deserialize, PartialEq)]
-struct Entry {
-    ent_seq: u64,
-    k_ele: Vec<KEle>,
-    r_ele: Vec<REle>,
-    sense: Vec<Sense>,
-}
-#[derive(Debug, Deserialize, PartialEq)]
-struct JMDict {
-    entry: Vec<Entry>,
+fn child_elms<'a, 'input: 'a>(node: &roxmltree::Node<'a, 'input>) -> impl Iterator<Item=roxmltree::Node<'a, 'input>> {
+    node.children().filter(|child| child.is_element())
 }
 fn main() {
     let text = std::fs::read_to_string("../JMdict_e").unwrap();
-    let dict: JMDict = quick_xml::de::from_str(&text).unwrap();
+    let doc = roxmltree::Document::parse_with_options(&text,
+        roxmltree::ParsingOptions { allow_dtd: true, ..roxmltree::ParsingOptions::default() })
+        .unwrap();
+    let root_elm = doc.root().first_element_child().unwrap();
+    assert!(root_elm.has_tag_name("JMdict"));
+    for entry in child_elms(&root_elm) {
+        println!("{:?}", entry);
+        assert!(entry.has_tag_name("entry"));
+
+    }
 }
