f2f35c330733857932d8de5e387c5fb5ad161974
comex <comexk@gmail.com>
2023-08-27 13:37:40 -0400

    update Yams

--- rerere/Package.swift
+++ rerere/Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 let package = Package(
     name: "rerere",
     dependencies: [
-        .package(url: "https://github.com/jpsim/Yams.git", from: "2.0.0"),
+        .package(url: "https://github.com/jpsim/Yams.git", from: "5.0.6"),
         .package(url: "https://github.com/apple/swift-argument-parser", from: "1.0.0"),
     ],
     targets: [
